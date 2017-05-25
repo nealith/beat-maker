1999b3c915b010b7816cc349cdb904e4e2074c54
nealith <nealith@neaworld.fr>
2017-05-25 18:33:29 +0200

    Implement Consumer

---
+++ src/components/base/Consumer.cpp
@@ -0,0 +1,36 @@
+#include <Consumer.h>
+#include <cassert>
+
+unsigned int Consumer::nbInput() const{
+    return m_nbInput;
+}
+
+std::shared_ptr<Pipe> Consumer::getInput(unsigned int noInput) const{
+    assert("This input doesn't exist" && noInput < m_nbInput);
+    return m_input[noInput];
+}
+
+void Consumer::connectInput(const std::shared_ptr<Pipe> p,unsigned int noInput){
+    assert("This input doesn't exist" && noInput < m_nbInput);
+    m_input[noInput] = p;
+}
+
+bool Consumer::thereAreSamples() const{
+    bool ret = true;
+    for (std::vector<std::shared_ptr<Pipe>>::const_iterator  it = m_input.begin(); (it != m_input.end() && ret); it++) {
+        if ((*it)->empty()) {
+            ret = false;
+        }
+    }
+    return ret;
+}
+
+Consumer::Consumer(unsigned int nbInput = 1):
+    m_nbInput(nbInput),m_input(nbInput,std::shared_ptr<Pipe>(NULL)){
+
+}
+
+double Consumer::read(unsigned int noInput){
+    assert("This input doesn't exist" && noInput < m_nbInput);
+    return m_input[nbInput]->extract();
+}
